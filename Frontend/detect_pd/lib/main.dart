1063517cd558d48757b0ba39a0d1daf615d06018
malith <pmkulathilake@gmail.com>
2021-02-04 20:47:30 +0530

    NegativeResultBackground

--- Frontend/detect_pd/lib/main.dart
+++ Frontend/detect_pd/lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(PositiveBackground());
 
 class PositiveBackground extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage("lib/images/PositiveBackground2.png"),
+                image: AssetImage("images/NegativeBackground2.png"),
                 fit: BoxFit.cover)),
         child: Scaffold(
           backgroundColor: Colors.transparent,
@@ -31,69 +32,6 @@ class PositiveBackground extends StatelessWidget {
   }
 }
 
-// void main() => runApp(MaterialApp(
-//   home: Text("Hey ninjas!"),
-//
-// ));
-
-// void main() => runApp(BaseLayout());
-//
-// class BaseLayout extends StatelessWidget{
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Scaffold(
-//             appBar: AppBar(
-//                 title: Text('Set Full Screen Background Image')),
-//             body: Center(
-//
-//             )
-//         )
-//     );
-//   }
-// }
-
-// class MyApp extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Scaffold(
-//             // appBar: AppBar(
-//             //     title: Text('Set Full Screen Background Image')),
-//             body: Center(
-//
-//                 child: Container(
-//                     constraints: BoxConstraints.expand(),
-//                     decoration: BoxDecoration(
-//                         image: DecorationImage(
-//                             image: AssetImage(""),
-//                             fit: BoxFit.cover)
-//                     ),
-//                     child: Center(child: Text('Set Full Screen Background Image in Flutter',
-//                       textAlign: TextAlign.center, style:
-//                       TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),)
-//                 )
-//             )
-//         )
-//     );
-//   }
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         decoration: BoxDecoration(
-//             image: DecorationImage(
-//                 image: AssetImage("lib/images/confetti.png"),
-//                 fit: BoxFit.cover
-//           )
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 
 
