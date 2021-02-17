bc617dbfd22a6950e1cb1419265e2d8584c7b58b
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-02-17 21:26:24 +0530

    fixed the issues with the variable names

--- Backend/DetectPD/User.py
+++ Backend/DetectPD/User.py
@@ -2,32 +2,32 @@ from .TestImage import TestImage
 
 
 class User:
-    def __init__(self, __testImage, __age, __gender, __handedness):
-        self.__testImage: TestImage = __testImage
+    def __init__(self, __test_image, __age, __gender, __handedness):
+        self.__test_image: TestImage = __test_image
         self.__age: int = __age
         self.__gender: str = __gender
         self.__handedness: str = __handedness
 
-        def get_age() -> int:
+        def get_age(self) -> int:
             return self.__age
 
-        def set_age(__age):
+        def set_age(self, __age):
             self.__age = __age
 
-        def get_gender() -> str:
+        def get_gender(self) -> str:
             return self.__gender
 
-        def set_gender(__gender):
+        def set_gender(self, __gender):
             self.__gender = __gender
 
-        def get_handedness() -> str:
+        def get_handedness(self) -> str:
             return self.__handedness
 
-        def set_handedness(__handedness):
+        def set_handedness(self, __handedness):
             self.__handedness = __handedness
 
-        def get_testImage() -> TestImage:
-            return __testImage
+        def get_testImage(self) -> TestImage:
+            return self.__test_image
 
-        def set_testImage(__testImage):
-            self.__testImage = __testImage
+        def set_testImage(self, __test_image: TestImage):
+            self.__test_image = __test_image
