4d44b2973045c23cf37cdde3993bfdbb3c23ecbe
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-02-17 21:34:09 +0530

    fixed issues with the variable names

--- Backend/DetectPD/User.py
+++ Backend/DetectPD/User.py
@@ -1,12 +1,12 @@
-from .TestImage import TestImage
+from TestImage import TestImage
 
 
 class User:
     def __init__(self, __test_image, __age, __gender, __handedness):
         self.__test_image: TestImage = __test_image
         self.__age: int = __age
-        self.__gender: str = __gender
-        self.__handedness: str = __handedness
+        self.__gender: int = __gender
+        self.__handedness: int = __handedness
 
         def get_age(self) -> int:
             return self.__age
@@ -14,20 +14,20 @@ class User:
         def set_age(self, __age):
             self.__age = __age
 
-        def get_gender(self) -> str:
+        def get_gender(self) -> int:
             return self.__gender
 
         def set_gender(self, __gender):
             self.__gender = __gender
 
-        def get_handedness(self) -> str:
+        def get_handedness(self) -> int:
             return self.__handedness
 
         def set_handedness(self, __handedness):
             self.__handedness = __handedness
 
-        def get_testImage(self) -> TestImage:
+        def get_test_image(self) -> TestImage:
             return self.__test_image
 
-        def set_testImage(self, __test_image: TestImage):
+        def set_test_image(self, __test_image: TestImage):
             self.__test_image = __test_image
