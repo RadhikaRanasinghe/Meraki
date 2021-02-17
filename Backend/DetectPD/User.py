723ed7571dbec5b428a94e484cc236f887ac3cc4
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-02-17 21:40:31 +0530

    added the input types

--- Backend/DetectPD/User.py
+++ Backend/DetectPD/User.py
@@ -11,19 +11,19 @@ class User:
         def get_age(self) -> int:
             return self.__age
 
-        def set_age(self, __age):
+        def set_age(self, __age: int):
             self.__age = __age
 
         def get_gender(self) -> int:
             return self.__gender
 
-        def set_gender(self, __gender):
+        def set_gender(self, __gender: int):
             self.__gender = __gender
 
         def get_handedness(self) -> int:
             return self.__handedness
 
-        def set_handedness(self, __handedness):
+        def set_handedness(self, __handedness: int):
             self.__handedness = __handedness
 
         def get_test_image(self) -> TestImage:
