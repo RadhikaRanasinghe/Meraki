11bf50dc23ba148b9e13a2291bf773962b486036
RadhikaRanasinghe <52253513+RadhikaRanasinghe@users.noreply.github.com>
2021-02-21 12:17:54 +0530

    Navigation bar dart file re added

--- Frontend/detect_pd/lib/navbar.dart
+++ Frontend/detect_pd/lib/navbar.dart
@@ -10,37 +10,71 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20.0),
-                    topLeft: Radius.circular(20.0)),
-                color: Colors.white,
+          body: Stack(
+            children: [
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: NavBar(
+                  link1: null,
+                  link2: null,
+                  link3: null,
+                )
               ),
-              height: 60.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const <Widget>[
-                  Icon(
-                    Icons.help_outline,
-                    color: Colors.black,
-                    size: 34.0,
-                  ),
-                  Icon(
-                    Icons.home_outlined,
-                    color: Colors.black,
-                    size: 34.0,
-                  ),
-                  Icon(
-                    Icons.settings_outlined,
-                    color: Colors.black,
-                    size: 34.0,
-                  ),
-                ],
-              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
+
+class NavBar extends StatelessWidget {
+  final link1;
+  final link2;
+  final link3;
+
+  NavBar({this.link1, this.link2, this.link3});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 60.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(20.0)),
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FlatButton(
+            onPressed: link1 ,
+            child: Icon(
+              Icons.help_outline,
+              color: Colors.black,
+              size: 34.0,
             ),
-          )),
+          ),
+          FlatButton(
+            onPressed: link2,
+            child: Icon(
+              Icons.home_outlined,
+              color: Colors.black,
+              size: 34.0,
+            ),
+          ),
+          FlatButton(
+            onPressed: link3,
+            child: Icon(
+              Icons.settings_outlined,
+              color: Colors.black,
+              size: 34.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
