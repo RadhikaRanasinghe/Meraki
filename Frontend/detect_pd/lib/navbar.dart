f3b456b1719afa0d0df7382b4f1ecfc6fb7af663
RadhikaRanasinghe <52253513+RadhikaRanasinghe@users.noreply.github.com>
2021-02-22 20:39:49 +0530

    Code clean-up

--- Frontend/detect_pd/lib/navbar.dart
+++ Frontend/detect_pd/lib/navbar.dart
@@ -1,6 +1,3 @@
-import 'HelpNavBarTest.dart';
-import 'HomePage.dart';
-import 'SettingsNavBarTest.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,24 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // routes: {
-      //   '/':(context) => Home(),
-      //   '/help':(context) => HelpPage(),
-      //   '/settings':(context) => SettingsPage(),
-      // },
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Stack(
           children: [
             Positioned(
                 bottom: 0,
-                left: 0,
-                child: NavBar(
-                  link1: null,
-                  link2: null,
-                  link3: null,
-                  backgroundColor: Colors.redAccent,
-                )
+              left: 0,
+              child: NavBar(
+                link1: null,
+                link2: null,
+                link3: null,
+                backgroundColor: Colors.redAccent,
+              ),
             ),
           ],
         ),
@@ -42,6 +34,9 @@ class NavBar extends StatelessWidget {
   final link1;
   final link2;
   final link3;
+  final iconSize = 40.0;
+  final height = 80.0;
+  final borderRadius = 40.0;
 
   NavBar({this.link1, this.link2, this.link3, this.backgroundColor});
 
@@ -51,11 +46,11 @@ class NavBar extends StatelessWidget {
       color: backgroundColor,
       child: Container(
         width: MediaQuery.of(context).size.width,
-        height: 80.0,
+        height: height,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
-              topRight: Radius.circular(40.0),
-              topLeft: Radius.circular(40.0)),
+              topRight: Radius.circular(borderRadius),
+              topLeft: Radius.circular(borderRadius)),
           color: Colors.white,
           boxShadow: [
             BoxShadow(
@@ -70,39 +65,27 @@ class NavBar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             FlatButton(
-              onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HelpPage()));
-              } ,
+              onPressed: link1,
               child: Icon(
                 Icons.help_outline,
                 color: Colors.black,
-                size: 40.0,
+                size: iconSize,
               ),
             ),
             FlatButton(
-              onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Homepage()));
-              },
+              onPressed: link2,
               child: Icon(
                 Icons.home_outlined,
                 color: Colors.black,
-                size: 40.0,
+                size: iconSize,
               ),
             ),
             FlatButton(
-              onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SettingsPage()));
-              },
+              onPressed: link3,
               child: Icon(
                 Icons.settings_outlined,
                 color: Colors.black,
-                size: 40.0,
+                size: iconSize,
               ),
             ),
           ],
@@ -111,68 +94,3 @@ class NavBar extends StatelessWidget {
     );
   }
 }
-
-// class NavBar extends StatelessWidget {
-//   final link1;
-//   final link2;
-//   final link3;
-//
-//   NavBar({this.link1, this.link2, this.link3});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: MediaQuery.of(context).size.width,
-//       height: 80.0,
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.only(
-//             topRight: Radius.circular(40.0),
-//             topLeft: Radius.circular(40.0)),
-//         color: Colors.white,
-//         boxShadow: [
-//           BoxShadow(
-//             color: Colors.black.withOpacity(0.3),
-//             spreadRadius: 1,
-//             blurRadius: 1,
-//             offset: Offset(0, -3),
-//           ),
-//         ],
-//       ),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: [
-//           FlatButton(
-//             onPressed: link1,
-//             child: Icon(
-//               Icons.help_outline,
-//               color: Colors.black,
-//               size: 40.0,
-//             ),
-//           ),
-//           FlatButton(
-//             onPressed: link2,
-//             child: Icon(
-//               Icons.home_outlined,
-//               color: Colors.black,
-//               size: 40.0,
-//             ),
-//             // shape: RoundedRectangleBorder(
-//             //   borderRadius: BorderRadius.circular(18.0),
-//             //   side: BorderSide(color: Colors.red)
-//             // ),
-//           ),
-//           FlatButton(
-//             onPressed: link3,
-//             child: Icon(
-//               Icons.settings_outlined,
-//               color: Colors.black,
-//               size: 40.0,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
