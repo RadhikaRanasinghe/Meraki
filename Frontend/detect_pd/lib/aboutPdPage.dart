f4f21b2ae96565860540fa5fa7a1bdcd914e4720
malithk911 <78416064+malithk911@users.noreply.github.com>
2021-02-25 00:50:35 +0530

    aboutPD page edited

--- Frontend/detect_pd/lib/aboutPdPage.dart
+++ Frontend/detect_pd/lib/aboutPdPage.dart
@@ -5,14 +5,14 @@ import 'home-background.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: AboutPD(),
+      home: AboutPDPage(),
       theme: ThemeData(
         canvasColor: Color.fromRGBO(118, 176, 195, 100),
       )
   ));
 }
 
-class AboutPD extends StatelessWidget {
+class AboutPDPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,22 +27,15 @@ class AboutPD extends StatelessWidget {
           backgroundColor: Color.fromRGBO(94, 163, 184, 100),
         ),
         appBarBackgroundColor:Color.fromRGBO(118, 176, 195, 100),
-        fillChild: Center(
-          child: Container(
-            child: Text("Parkinson's Disease \n\nLorem Ipsum is simply dummy "
-                "text of the printing and typesetting industry. Would you "
-                "like to take the guided test or continue? \n\nLorem Ipsum is "
-                "simply dummy text of the printing and typesetting industry."
-                "Would you like to take the guided test or continue?",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
-            margin: EdgeInsets.all(30.0),
-            padding: EdgeInsets.all(10.0),
-            alignment: Alignment.topCenter,
-          ),
+        fillChild: AboutPD(
+          description: "Parkinson's Disease \n\nLorem Ipsum is simply dummy "
+              "text of the printing and typesetting industry. Would you "
+              "like to take the guided test or continue? \n\nLorem Ipsum is "
+              "simply dummy text of the printing and typesetting industry."
+              "Would you like to take the guided test or continue?",
+          fontSize: 20,
+          alignment: Alignment.topCenter,
+          textColor: Colors.white,
         ),
         // fillColor:  Color.fromRGBO(240, 241, 226, 100),
         fillColor:  Color(0xff033e6b),
@@ -52,6 +45,32 @@ class AboutPD extends StatelessWidget {
         link2: null,
         link3: null,
         backgroundColor: Color.fromRGBO(240, 241, 226, 100) ,
+      ),
+    );
+  }
+}
+
+class AboutPD extends StatelessWidget {
+  final description;
+  final fontSize;
+  final alignment;
+  final textColor;
+
+  const AboutPD({this.description, this.fontSize, this.alignment, this.textColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text(description,
+          style: TextStyle(
+            color: textColor,
+            fontSize: fontSize,
+          ),
+        ),
+        margin: EdgeInsets.all(30.0),
+        padding: EdgeInsets.all(10.0),
+        alignment: alignment,
       ),
     );
   }
