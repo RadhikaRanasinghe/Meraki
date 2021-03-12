422f4f32b4ffbeea77bc52ea894ee6d3d9c71e20
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-03-12 16:37:27 +0530

    load features method updated

--- Backend/DetectPD/Detector.py
+++ Backend/DetectPD/Detector.py
@@ -1,26 +1,45 @@
 from User import User
 from UserModel import UserModel
+from TestImageBuilder import TestImageBuilder
+import io
+import base64
+import PIL.Image as Image
+import os
+
+__user: User = None
 
 
 class Detector:
-    __user: User = None
 
     def __init__(self):
         pass
 
     def load_features(self, image_no: UserModel):
         # TODO: GET the image from the database.
+        b = base64.b64encode(UserModel.get_test_image())
+        img = Image.open(io.BytesIO(b))
+        img.save('/images/image' + str(UserModel.get_test_image_id()) + '.png')
 
         # TODO: Run the C++ file.
 
         # TODO: Read RMS.txt(assume its a csv).
+        os.remove('/images/image' + str(UserModel.get_test_image_id()) + '.png')
+        feature_file = open("RMS.txt", "r")
+        features = feature_file.read()
+        features = features.split(" ")
 
         # TODO: Fill the __user variable.
+        test_image = TestImageBuilder.build(features[0], features[1], features[2], features[3], features[4],
+                                            features[5],
+                                            features[6], features[7], features[8])
 
-        pass
+        self.__user.set_test_image(test_image)
+        self.__user.set_age(image_no.get_age())
+        self.__user.set_gender(image_no.get_gender())
+        self.__user.set_handedness(image_no.get_handedness())
 
     def process(self, image_no: int) -> bool:
-        result: bool = True
+        result: bool = None
 
         # TODO: Run data science .py file or do it here.
 
