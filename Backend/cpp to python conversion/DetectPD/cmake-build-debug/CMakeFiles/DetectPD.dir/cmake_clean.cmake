8f9a8051317e9f2180b996911fe7d5d9be408304
RadhikaRanasinghe <52253513+RadhikaRanasinghe@users.noreply.github.com>
2021-03-23 13:27:54 +0530

    Adding the source code with CMake.txt

---
+++ Backend/cpp to python conversion/DetectPD/cmake-build-debug/CMakeFiles/DetectPD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DetectPD.dir/main.cpp.obj"
+  "DetectPD.exe"
+  "DetectPD.exe.manifest"
+  "DetectPD.lib"
+  "DetectPD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DetectPD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
