3fd31aab9883c7d6dd12b956d2065a0969e40f0d
RadhikaRanasinghe <52253513+RadhikaRanasinghe@users.noreply.github.com>
2021-03-28 09:22:05 +0530

    Commenting the code

--- Backend/Models/RandomForest/RandomForest.py
+++ Backend/Models/RandomForest/RandomForest.py
@@ -1,3 +1,4 @@
+# importing the libraries
 import numpy as np
 import pandas as pd
 import sklearn
@@ -7,9 +8,10 @@ from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.metrics import accuracy_score, classification_report
 
+# Loading the dataset with a Pandas and the returned data frame is caught by data variable
 data = pd.read_csv("data/DetectPD-SMOTE.csv")
 
-# Preprocessing
+# Preprocessing using the LabelEncoder
 le = sklearn.preprocessing.LabelEncoder()
 gender = le.fit_transform(list(data['GENDER']))
 handedness = le.fit_transform(list(data['RIGH/LEFT-HANDED']))
@@ -30,48 +32,66 @@ x = list(zip(gender, handedness, age, rms, max_st_ht, min_st_ht, std_st_ht, mrt,
              n_to_p_st_ht))
 y = list(class_type)
 
+# Adding the 'x' and 'y' to Numpy Arrays
 x = np.array(x)
 y = np.array(y)
 
+# Splitting the data into testing data and training data with the testing size of 0.1
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
+# The Random Forest Classifier Model is assigned to the model variable
 model = RandomForestClassifier()
+
+# The training data from the data split is taken for fitting into the model to train
 model.fit(x_train, y_train)
+
+# The accuracy score of the model taken as percentage is printed on the console
 print(model.score(x_test, y_test) * 100)
 
+# Initializing and assigning the variables for storing the best 3 and worst pickle file
 best_result1_file = "DetectPD_SMOTE/RF_BestModel1_SMOTE.pickle"
 best_result2_file = "DetectPD_SMOTE/RF_BestModel2_SMOTE.pickle"
 best_result3_file = "DetectPD_SMOTE/RF_BestModel3_SMOTE.pickle"
 worst_result_file = "DetectPD_SMOTE/RF_WorstModel_SMOTE.pickle"
 
+# Initializing and assigning the variables for storing the best 3 and worst accuracies
 best_result1 = 0
 best_result2 = 0
 best_result3 = 0
 worst_result = 100
 hundred_count = 0
 
-best_model1 = 0
+# Initializing and assigning the variables for storing the strings of the reports generated
 best_model1_report = ""
-best_model2 = 0
 best_model2_report = ""
-best_model3 = 0
 best_model3_report = ""
-worst_model = 0
 worst_model_report = ""
 
+# Iterating for 10,000 times to get the best the models
 for i in range(10000):
+    # Splitting the data into testing data and training data with the testing size of 0.1
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+
+    # The Random Forest Classifier Model is assigned to the model variable
     model = RandomForestClassifier()
+
+    # The training data from the data split is taken for fitting into the model to train
     model.fit(x_train, y_train)
+
+    # Predicting using the testing data
     predicted = model.predict(x_test)
 
+    # The accuracy score of the model taken as percentage
     acc = model.score(x_test, y_test) * 100
 
+    # Printing the iteration number and the accuracy score as a percentage
     print("Iteration " + str(i) + ": " + str(acc))
 
+    # Counting number of hundreds in the 10,000 iterations
     if acc == 100:
         hundred_count += 1
 
+    # If the accuracy is greater than any result stored, store the model as the best_model1 and the save the pickle file
     if acc > best_result1 or acc > best_result2 or acc > best_result3:
         if acc > best_result1:
             best_result1 = acc
@@ -79,6 +99,8 @@ for i in range(10000):
             with open(best_result1_file, 'wb') as file:
                 pickle.dump(model, file)
 
+        # If the accuracy is greater than the best_result2, store the model as the best_model2
+        # and the save the pickle file
         elif acc > best_result2:
             if best_result1 > acc:
                 best_result2 = acc
@@ -87,7 +109,8 @@ for i in range(10000):
                     pickle.dump(model, file)
             else:
                 pass
-
+        # If the accuracy the less the best_result1 and best_result2, store the model as the best_model3
+        # and the save the pickle file
         else:
             if acc < best_result1 and acc < best_result2:
                 best_result3 = acc
@@ -97,13 +120,17 @@ for i in range(10000):
             else:
                 pass
 
+    # If the accuracy is greater than the worst_result, store the model as the worst_model and the save the pickle file
     if acc < worst_result:
         with open(worst_result_file, 'wb') as file:
             pickle.dump(model, file)
         worst_result = acc
         worst_model_report = sklearn.metrics.classification_report(y_test, predicted)
 
+    # Opening the file to write into
     file1 = open("DetectPD_SMOTE/RF_results_SMOTE.md", "w")  # write mode
+
+    # Writing to a file accuracies and their classification reports
     file1.write("\n######**_The Best Model 1_**: \n\nAccuracy: " + str(best_result1) + "\n\n" + best_model1_report)
     file1.write("\n######**_The Best Model 2_**: \n\nAccuracy: " + str(best_result2) + "\n\n" + best_model2_report)
     file1.write("\n######**_The Best Model 3_**: \n\nAccuracy: " + str(best_result3) + "\n\n" + best_model3_report)
@@ -111,6 +138,7 @@ for i in range(10000):
     file1.write("\n\n######**_The number of models with 100% accuracy in 10,000 iterations_**:" + str(hundred_count))
     file1.close()
 
+# Printing the best and the worst models as a summary
 print("Best result A", best_result1)
 print("Best result B", best_result2)
 print("Best results C", best_result3)
