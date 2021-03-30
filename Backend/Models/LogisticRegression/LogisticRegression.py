6bfa48ead936ed827906efb598212254f62d0ae8
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-03-30 13:45:01 +0530

    made some changes to the comments

--- Backend/Models/LogisticRegression/LogisticRegression.py
+++ Backend/Models/LogisticRegression/LogisticRegression.py
@@ -1,3 +1,4 @@
+# importing the libraries
 import pandas as pd
 import sklearn
 from sklearn.preprocessing import LabelEncoder
@@ -7,9 +8,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import pickle
 
+# Loading the dataset with a Pandas and the returned data frame is caught by data variable
 pd_data = pd.read_csv('DetectPD.csv')
 
-# creating dummy data
+# preprocessing the dataset using the LabelEncoder
 le = sklearn.preprocessing.LabelEncoder()
 gender = le.fit_transform(list(pd_data['GENDER']))
 handedness = le.fit_transform(list(pd_data['RIGH/LEFT-HANDED']))
@@ -30,10 +32,11 @@ X = list(zip(gender, handedness, age, rms, max_st_ht, min_st_ht, std_st_ht, mrt,
              n_to_p_st_ht))
 y = list(class_type)
 
+# Adding the 'x' and 'y' to Numpy Arrays
 X = np.array(X)
 y = np.array(y)
 
-# initializing variables
+# Initializing and assigning the variables for storing the strings of the reports generated
 lowestAccuracy = 0
 highestAccuracy1 = 0
 highestAccuracy2 = 0
@@ -43,15 +46,20 @@ highModel1 = 0
 highModel2 = 0
 highModel3 = 0
 
+# Iterating for 10,000 times to get the best the models
 for x in range(10000):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)  # splitting test size and train size
+    # Splitting the data into testing data and training data with the testing size of 0.1
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    # The Logistic Regression Model is assigned to the logmodel variable
     logmodel = LogisticRegression(solver='lbfgs', max_iter=20000)
-    logmodel.fit(X_train, y_train)  # fitting  the training data into the model
-    predictions = logmodel.predict(X_test)  # predicting using the model
-    classification_report(y_test, predictions)
-    confusion_matrix(y_test, predictions)
-    modelAccuracy = accuracy_score(y_test, predictions) * 100  # getting the accuracy score for the prediction
-    if x == 0:  # if its the first iteration
+    # The training data from the data split is taken for fitting into the model to train
+    logmodel.fit(X_train, y_train)
+    # predicting using the model
+    predictions = logmodel.predict(X_test)
+    # getting the accuracy score for the prediction
+    modelAccuracy = accuracy_score(y_test, predictions) * 100
+    # if its the first iteration
+    if x == 0:
         lowModel = logmodel
         highModel1 = logmodel
         highModel2 = lowModel
@@ -60,23 +68,28 @@ for x in range(10000):
         highestAccuracy1 = modelAccuracy
         highestAccuracy2 = modelAccuracy
         highestAccuracy3 = modelAccuracy
-        file = open("log.txt", "w")  # creating anf writing the accuracies to a log file
+        # creating and writing the accuracies to a log file
+        file = open("log.txt", "w")
         file.write("Highest accuracy 1 : " + str(highestAccuracy1) +
                    "\n Highest accuracy 2 : " + str(highestAccuracy2) +
                    "\n Highest accuracy 3 : " + str(highestAccuracy3) +
                    "\n Lowest accuracy : " + str(lowestAccuracy))
         file.close()
     else:
-        if modelAccuracy < lowestAccuracy:  # if the current model accuracy is lover than the lowest accuracy
+        # if the current model accuracy is lover than the lowest accuracy
+        if modelAccuracy < lowestAccuracy:
             lowestAccuracy = modelAccuracy
             lowModel = logmodel
             file = open("log.txt", "a")
             file.write(" \n Lowest accuracy : " + str(lowestAccuracy))
             file.close()
-        elif modelAccuracy > highestAccuracy3:  # if the current model accuracy is higher than the 3rd highest accuracy
+            # if the current model accuracy is higher than the 3rd highest accuracy
+        elif modelAccuracy > highestAccuracy3:
             file = open("log.txt", "a")
-            if modelAccuracy > highestAccuracy2:  # if the current model accuracy is higher than the 2ndrd highest accuracy
-                if modelAccuracy > highestAccuracy1:  # if the current model accuracy is higher than the 1st highest accuracy
+            # if the current model accuracy is higher than the 2nd highest accuracy
+            if modelAccuracy > highestAccuracy2:
+                # if the current model accuracy is higher than the 1st highest accuracy
+                if modelAccuracy > highestAccuracy1:
                     highestAccuracy1 = modelAccuracy
                     highModel1 = logmodel
                     file.write("\n Highest 1 accuracy : " + str(highestAccuracy1))
