f27d7a2127e232d561d2461dfa9c28b1da6f8f41
malithk911 <78416064+malithk911@users.noreply.github.com>
2021-04-11 23:41:35 +0530

    Comments and code clean-up showcaseview.dart

--- Frontend/detect_pd/lib/views/widgets/showcaseview.dart
+++ Frontend/detect_pd/lib/views/widgets/showcaseview.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:showcaseview/showcase.dart';
 
 class CustomShowcaseWidget extends StatelessWidget {
-  final GlobalKey globalKey;
+  final GlobalKey globalKey; // Global key to identify widget
   final String description;
   final Widget child;
 
@@ -18,18 +18,14 @@ class CustomShowcaseWidget extends StatelessWidget {
 
     showcaseBackgroundColor: Colors.teal[300],
     contentPadding: EdgeInsets.all(25),
-    // showArrow: false,
     disableAnimation: false,
-    // title: 'Hello',
-    // titleTextStyle: TextStyle(color: Colors.white, fontSize: 32),
     description: description,
     descTextStyle: TextStyle(
       color: Colors.indigo[900],
       fontWeight: FontWeight.bold,
       fontSize: 16,
-    ),
-    // overlayColor: Colors.white,
+    ), // TextStyle
     overlayOpacity: 0.0,
     child: child,
-  );
+  ); // Showcase
 }
