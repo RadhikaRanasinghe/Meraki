61afb3e5feb5e56d1355db64c12c5447bae18d3c
CharinduLiyanage <charinduliyanage@yahoo.com>
2021-04-14 18:07:47 +0530

    error message updated

--- Backend/DetectPD/FrontendController.py
+++ Backend/DetectPD/FrontendController.py
@@ -51,9 +51,10 @@ def create_user():
             # Returning the primary key of the RDS database record.
             return jsonify({"image_no": user_id}), 201
         else:
-            return jsonify({"error": "Invalid input type"}), 415
+            message = "Invalid input type. \n'age'/'gender'/'handedness' - Integer, \n'image' - jpg/jpeg image"
+            return jsonify({"error": message}), 415
     else:
-        return jsonify({"error": "Missing input"}), 400
+        return jsonify({"error": "Missing input. \n'age', 'gender', 'handedness', 'image' is required."}), 400
 
 
 @application.route('/retrieve_result', methods=['GET'])
@@ -100,11 +101,11 @@ def retrieve_result():
                     # Return the test result.
                     return jsonify({"result": result}), 200
             else:
-                return jsonify({"error": "No such id exists in the database"}), 416
+                return jsonify({"error": "Invalid image_no, No such id exists in the database."}), 416
         else:
-            return jsonify({"error": "Invalid input type"}), 415
+            return jsonify({"error": "Invalid input type. \n'image_no' - Integer"}), 415
     else:
-        return jsonify({"error": "Missing input"}), 400
+        return jsonify({"error": "Missing input. \n'image_no' is required."}), 400
 
 
 if __name__ == '__main__':
