5697306f68b726c5f73e775710876ba4b3bcdf57
CharinduLiyanage <charinduliyanage@yahoo.com>
2021-04-18 17:56:42 +0530

    Update main.py

--- Backend/Data/main.py
+++ Backend/Data/main.py
@@ -2,7 +2,7 @@ import image_to_csv
 from writing import combine_personal_datasets, create_oversampling
 
 # Change to uer name to before running. (AMIRU/BAWANTHA/CHANAKA/CHARINDU/MALITH/RADHIKA)
-user = image_to_csv.MALITH
+user = image_to_csv.CHARINDU
 image_to_csv.convert(user)
 
 
