d5209498f5ae56aeeb3ffdf2bf6774c3906f50ef
BawanthaLaptop <bawanthaperera1999@gmail.com>
2021-04-19 14:54:26 +0530

    Revert "pickle files added for SVM"
    This reverts commit f6a5e903b90dafa964f9334ea02ed5d9c015ab26.

--- Backend/Models/SVM/Results/LinearSVC.py
+++ Backend/Models/SVM/Results/LinearSVC.py
@@ -1,17 +1,21 @@
 # importing the libraries
 import pandas as pd
+import sklearn
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.metrics import accuracy_score
+from sklearn import svm, metrics
+from sklearn.metrics import accuracy_score, classification_report
 import pickle
 from sklearn.svm import LinearSVC
 
 # reading the csv file
-pd_data = pd.read_csv('DetectPD_SMOTE.csv')
+pd_data = pd.read_csv('DetectPD-SMOTE.csv')
 
-gender = list(pd_data['GENDER'])
-handedness = list(pd_data['RIGH/LEFT-HANDED'])
+# pre processing using Label Encoder
+le = sklearn.preprocessing.LabelEncoder()
+gender = le.fit_transform(list(pd_data['GENDER']))
+handedness = le.fit_transform(list(pd_data['RIGH/LEFT-HANDED']))
 age = list(pd_data['AGE'])
 rms = list(pd_data['RMS'])
 max_st_ht = list(pd_data['MAX_BETWEEN_ST_HT'])
@@ -43,9 +47,9 @@ highCount = 0
 for x in range(10000):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
     clf = LinearSVC(penalty='l2', C=1.0, random_state=None, dual=False, max_iter=10000)
-    clf.fit(X_train, y_train)  # fitting  the training data into the model
+    clf.fit(X_train, y_train)   # fitting  the training data into the model
     y_predict = clf.predict(X_test)
-    accuracy = accuracy_score(y_test, y_predict) * 100  # getting accuracy as percentage
+    accuracy = accuracy_score(y_test, y_predict) * 100   # getting accuracy as percentage
     if x == 0:  # first iteration
         lowModel = clf
         highModel = clf
@@ -57,7 +61,6 @@ for x in range(10000):
             lowestAccuracy = accuracy
             lowModel = clf
             print("low model")
-            print("Accuracy is: " + str(accuracy))
             print(metrics.classification_report(y_test, y_predict))
 
         # Creating a high model
@@ -66,13 +69,12 @@ for x in range(10000):
             highestAccuracy = accuracy
             highModel = clf
             print("high model", highCount)
-            print("Accuracy is: " + str(accuracy))
             print(metrics.classification_report(y_test, y_predict))
             # writing the high model into the pickle file
-            with open('SVM_BestModel_SMOTE_%s.pickle' % highCount, 'wb') as high:
+            with open('SVM_BestModel_%s.pickle' % highCount, 'wb') as high:
                 pickle.dump(highModel, high, protocol=pickle.HIGHEST_PROTOCOL)
 
 # writing the lowest model into the pickle file
-with open('SVM_WorstModel_SMOTE.pickle', 'wb') as low:
+with open('SVM_WorstModel.pickle', 'wb') as low:
     pickle.dump(lowModel, low, protocol=pickle.HIGHEST_PROTOCOL)
     print('success')
