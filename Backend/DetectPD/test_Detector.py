dac3750427076aa45e3c6be14c946ad89d76b1c5
AmiruRandil <61134249+AmiruRandil@users.noreply.github.com>
2021-04-20 23:00:52 +0530

    added comments to the test_detector file

--- Backend/DetectPD/test_Detector.py
+++ Backend/DetectPD/test_Detector.py
@@ -7,28 +7,43 @@ from UserModel import UserModel
 
 
 class TestDetector(TestCase):
-    # __user = select_record_test(10)
-
-    # detector = Detector()
+    """
+    This class unit tests the Detector class
+    """
 
     def test_load_features(self):
+        """
+        This function is used to test if the User object is updated properly
+        """
+        # Creating an Detector object
         detector = Detector()
-        im = cv2.imread('images/exam_1_pen.jpg')
+        im = cv2.imread('sample_images/exam_1.jpg')
         is_success, im_buf_arr = cv2.imencode(".jpg", im)
         byte_image = im_buf_arr.tobytes()
+        # Creating the UserModel object
         user = UserModel(1, 31, 1, 0, byte_image, 10)
+        # Calling the loadFeatures function
         detector.load_features(user)
 
+        # Checking if the User object has been updated properly
         self.assertEqual(detector.get_user().get_handedness(), 0)
         self.assertEqual(detector.get_user().get_gender(), 1)
         self.assertEqual(detector.get_user().get_age(), 31)
 
     def test_process(self):
+        """
+        This function is used to test if prediction returned is correct or not
+        """
+        # Creating an Detector object
         detector = Detector()
-        im = cv2.imread('images/exam_1_pen.jpg')
+        im = cv2.imread('sample_images/exam_1.jpg')
         is_success, im_buf_arr = cv2.imencode(".jpg", im)
         byte_image = im_buf_arr.tobytes()
+        # Creating the UserModel object
         user = UserModel(1, 31, 1, 0, byte_image, 10)
+        # Calling the loadFeatures function
         detector.load_features(user)
+        # Getting the result
         result = detector.process()
-        self.assertEqual(result, True)
+        # Checking if the result is correct or not
+        self.assertEqual(result, False)
